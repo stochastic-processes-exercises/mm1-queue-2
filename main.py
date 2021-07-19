5f7585ca6ecad74229e24743e062c19a938ba23d
Gareth Tribello <gareth.tribello@gmail.com>
2021-07-19 11:55:33 +0100

    Added answers

--- main.py
+++ main.py
@@ -11,5 +11,38 @@ lam, expr = 0.5, 1.0
 N = 100000
 
 # Use this to hold the times when the queue changes length as is discussed in the text
-events = []
+def exp_rv( lamd ) :
+    return -np.log( np.random.uniform(0,1) ) / lamd
+
 # Add code here to simulate the queue and to determine the distribution of lengths
+events = []
+time, tqt, lst = 0, np.zeros(N), exp_rv( expr )
+events.append([lst,-1])
+for i in range(1,N) :
+    time = time + exp_rv( lam )
+    events.append([time,+1])
+    if lst<time : est = time  
+    else : est = lst 
+    lst = est + exp_rv( expr )
+    events.append([lst,-1])
+
+events.sort() 
+qlen, qstats = 1, []
+qstats.append([0,1])
+for time, change in events : 
+    qlen = qlen + change
+    qstats.append( [time, qlen] )
+
+distribution = np.zeros(9) 
+for i in range(1,len(qstats)) :
+    if qstats[i-1][1]>8 : continue 
+    time_with_len = qstats[i][0] - qstats[i-1][0]
+    distribution[ qstats[i-1][1] ] = distribution[ qstats[i-1][1] ] + time_with_len
+
+distribution = distribution / lst 
+
+xv = np.linspace(0,8,9)
+plt.bar( xv, distribution, width=0.1 )
+plt.xlabel("number of people in queue")
+plt.ylabel("probability")
+plt.savefig("mydist.png")
