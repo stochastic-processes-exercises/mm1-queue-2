90b2f0a740054cad1d3051e8e1ace6e5d07c43c6
Gareth Tribello <gareth.tribello@gmail.com>
2021-07-19 11:43:26 +0100

    Added tests

---
+++ main.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# These are the parameters of the queue that you should use.  If you change the values
+# of these parameters you will fail the test as I have set up the test this way.  In 
+# writing your project you can change them but you should always ensure that lam<expr
+lam, expr = 0.5, 1.0
+
+# Use this to hold the times when the queue changes length as is discussed in the text
+events = []
+# Add code here to simulate the queue and to determine the distribution of lengths
---
+++ testing/test_main.py
@@ -0,0 +1,36 @@
+try:
+    from AutoFeedback.plotchecks import check_plot
+except:
+    import subprocess
+    import sys
+
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "AutoFeedback"])
+    from AutoFeedback.plotchecks import check_plot
+
+from AutoFeedback.plotclass import line
+from AutoFeedback.randomclass import randomvar
+import unittest
+from main import *
+
+p = lam / expr
+x, e, v, bmin, bmax, isi = [], [], [], [], [], [] 
+for i in range(9) :
+    x.append(i) 
+    prob = (1-p)*(p**i)
+    e.append( prob )
+    v.append( prob*(1-prob) / N ) 
+    bmin.append(0) 
+    bmax.append(1)
+    isi.append(False)
+
+rvar = randomvar( e, variance=v, vmin=bmin, vmax=bmax, isinteger=isi )
+line1 = line( x, rvar  )
+axislabels=["number of people in queue", "probability"]
+
+class UnitTests(unittest.TestCase) :
+    def test_variables(self) : 
+      assert(vc.check_vars("lam",0.5))
+      assert(vc.check_vars("expr",1.0))
+  
+    def test_queue(self) :
+      assert(check_plot([],exppatch=line1,explabels=axislabels,explegend=False,output=True)) 
